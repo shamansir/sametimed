0180eb1e2b164a99521c535bad6dee097d78ffec
shamansir@shamansir-vm <shamansir@shamansir-vm>
2009-12-22 08:09:56 +0300

    again, file skipped by mercurial

---
+++ src/name/shamansir/sametimed/wave/doc/cursor/DocumentElementStartPosSearchingCursor.java
@@ -0,0 +1,43 @@
+package name.shamansir.sametimed.wave.doc.cursor;
+
+import org.waveprotocol.wave.model.document.operation.AnnotationBoundaryMap;
+import org.waveprotocol.wave.model.document.operation.Attributes;
+
+public class DocumentElementStartPosSearchingCursor implements
+		ICursorWithResult<Integer> {
+
+	public DocumentElementStartPosSearchingCursor(int pos) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Integer getResult() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void annotationBoundary(AnnotationBoundaryMap map) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void characters(String chars) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void elementEnd() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void elementStart(String type, Attributes attrs) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
