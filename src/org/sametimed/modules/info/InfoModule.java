5a16d7afd1c702f4f951fd01db7cbb00842baa29
shaman.sir <shaman.sir@gmail.com>
2010-04-10 20:02:51 +0300

    InfoModule mock-up + corrected modules configs

---
+++ src/org/sametimed/modules/info/InfoModule.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package org.sametimed.modules.info;
+
+import org.sametimed.module.SametimedModule;
+
+/**
+ * Project: sametimed
+ * Package: org.sametimed.modules.info
+ *
+ * InfoModule
+ *
+ * @author Ulric Wilfred <shaman.sir@gmail.com>
+ * @date Apr 10, 2010 7:57:15 PM 
+ *
+ */
+public class InfoModule extends SametimedModule {
+
+    protected InfoModule(final String id) { super(id); }    
+    
+}
